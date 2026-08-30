ca03e8b4ec4c88c2e3b3f3442fbb7fe763a36406
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2026-08-30 14:56:25 +0200

    load titus andronicus acts individually
    load each act separately instead of merging them, so I can compare each act against Shakespeare and Peele

--- stylometry_titus.py
+++ stylometry_titus.py
@@ -2,8 +2,7 @@ import os
 
 corpus = {
     'Peele': 'corpus/peele',
-    'Shakespeare': 'corpus/shakespeare',
-    'Disputed': 'corpus/disputed'
+    'Shakespeare': 'corpus/shakespeare'
 }
 
 def read_folder_into_string(path):
@@ -13,10 +12,23 @@ def read_folder_into_string(path):
             strings.append(f.read())
     return '\n'.join(strings)
 
+def read_folder_into_dict(path):
+    texts = {}
+    for filename in sorted(os.listdir(path)):
+        name = os.path.splitext(filename)[0]
+        with open(os.path.join(path, filename), 'r', encoding='utf-8') as f:
+            texts[name] = f.read()
+    return texts
+
 play_by_author = {}
 
 for author, plays in corpus.items():
     play_by_author[author] = read_folder_into_string(plays)
 
+titus_acts = read_folder_into_dict('corpus/disputed')
+
 for author in corpus:
-    print(play_by_author[author][:100])
+    print(author, '-', play_by_author[author][:100])
+
+for act in titus_acts:
+    print(act, '-', titus_acts[act][:100])
